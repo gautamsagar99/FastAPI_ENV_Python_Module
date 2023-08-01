c88a2822f6a1fa464078c1c449b25da41ed92b08
Gautam Sagar <gautamsagar.mallela99@gmail.com>
2023-07-31 19:58:17 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,6 @@
+from fastapienv import fastapienv
+
+# fastapi_generator.createFastApiEnvironment("myenv", activate_env=True, start_server=True, server_host="localhost", server_port=8000, server_reload=True)
+
+
+fastapienv.createFastApiEnvironment("myenv", activate_env=True)
